cdda7d924ac6b62552abfdf4e40a43624f3e06db
Walther Lalk <emailme@waltherlalk.com>
2014-08-17 15:03:13 +0200

    Enable croogo 2.0 compat.

---
+++ Config/Migration/1408275566_firstmigrationswitcher.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Walther
+ * Date: 2014-08-17
+ * Time: 01:39 PM
+ */
+
+class Firstmigrationswitcher extends CakeMigration {
+	/**
+	 * Actions to be performed
+	 *
+	 * @var array $migration
+	 * @access public
+	 */
+	public $migration = array(
+		'up' => array(
+			'create_table' => array(
+				'switcher_paths' =>	array(
+					'id' => array('type' => 'integer', 'null' => false, 'default' => NULL, 'length' => 20, 'key' => 'primary'),
+					'path' => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 64, 'key' => 'unique', 'collate' => 'utf8_unicode_ci', 'charset' => 'utf8'),
+					'layout' => array('type' => 'text', 'null' => false, 'default' => NULL, 'collate' => 'utf8_unicode_ci', 'charset' => 'utf8'),
+					'theme' => array('type' => 'string', 'null' => false, 'default' => NULL, 'collate' => 'utf8_unicode_ci', 'charset' => 'utf8'),
+					'indexes' => array('PRIMARY' => array('column' => 'id', 'unique' => 1), 'key' => array('column' => 'path', 'unique' => 1)),
+					'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_unicode_ci', 'engine' => 'MyISAM')
+				)
+			)
+		),
+		'down' => array(
+			'drop_table' => array(
+				'switcher_paths'
+			),
+		),
+	);
+} 
---
+++ Config/SwitcherActivation.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Walther
+ * Date: 2014-08-17
+ * Time: 01:41 PM
+ */
+
+class SwitcherActivation {
+
+/**
+ * onActivate will be called if this returns true
+ *
+ * @param  object $controller Controller
+ * @return boolean
+ */
+	public function beforeActivation(&$controller) {
+		return true;
+	}
+
+/**
+ * Called after activating the plugin in ExtensionsPluginsController::admin_toggle()
+ *
+ * @param object $controller Controller
+ * @return void
+ */
+	public function onActivation(&$controller) {
+		App::uses('CroogoPlugin', 'Extensions.Lib');
+		$CroogoPlugin = new CroogoPlugin();
+		$CroogoPlugin->migrate('Switcher');
+	}
+} 
